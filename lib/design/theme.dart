81f9324484b562c3114b72140776ab8ffd419887
Yoni <yonifram4@gmail.com>
2022-12-29 18:34:43 +0200

    Add auth

---
+++ lib/design/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Themes {
+  static ThemeData? normal = ThemeData(
+    colorScheme: const ColorScheme.light(
+      primary: Color.fromARGB(255, 67, 139, 217),
+      secondary: Color.fromARGB(255, 39, 52, 62),
+      background: Color.fromARGB(255, 225, 230, 235),
+    ),
+  );
+}
