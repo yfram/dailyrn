1c7a3a87087c703b922af978352372ccbd9d875a
Yoni <yonifram4@gmail.com>
2022-12-29 19:47:24 +0200

    Create welcome page UI

--- lib/design/theme.dart
+++ lib/design/theme.dart
@@ -2,11 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Themes {
+  static Color get primary => const Color.fromARGB(255, 67, 139, 217);
+  static Color get secondary => const Color.fromARGB(255, 39, 52, 62);
+  static Color get background => const Color.fromARGB(255, 225, 230, 235);
+
   static ThemeData? normal = ThemeData(
-    colorScheme: const ColorScheme.light(
-      primary: Color.fromARGB(255, 67, 139, 217),
-      secondary: Color.fromARGB(255, 39, 52, 62),
-      background: Color.fromARGB(255, 225, 230, 235),
-    ),
-  );
+      colorScheme: ColorScheme.light(
+        primary: primary,
+        secondary: secondary,
+        background: background,
+      ),
+      textTheme: GoogleFonts.rubikTextTheme(),
+      primaryTextTheme: GoogleFonts.bonaNovaTextTheme(),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(secondary),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          alignment: Alignment.center,
+          fixedSize: MaterialStateProperty.all<Size>(Size(300, 50)),
+        ),
+      ));
 }
--- lib/pages/welcome_screen.dart
+++ lib/pages/welcome_screen.dart
@@ -1,8 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Center(
+          child: Column(children: [
+        Padding(
+          padding: EdgeInsets.only(top: 70),
+          child: Image.asset('assets/images/logo.png', width: 190),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 30),
+          child: Text('ברוכים הבאים לדיילירן!',
+              style: Theme.of(context).primaryTextTheme.headline4),
+        ),
+        TextButton(
+            onPressed: () {},
+            child: const Text('הרשמה למערכת',
+                style: TextStyle(color: Colors.blue))),
+        const Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: SizedBox(
+            width: 300,
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'שם משתמש',
+              ),
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: SizedBox(
+            width: 300,
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'סיסמה',
+              ),
+            ),
+          ),
+        ),
+        Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: SizedBox(
+                width: 300,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      Text('התחברות'), // <-- Text
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Icon(
+                        // <-- Icon
+                        Icons.login,
+                      ),
+                    ],
+                  ),
+                ))),
+        Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: SizedBox(
+              height: 50,
+              width: 300,
+              child: SignInButton(
+                Buttons.Google,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                text: "   התחברות עם גוגל",
+                onPressed: () {},
+              ),
+            )),
+      ])),
+    );
   }
 }
